0501153f4bb5b020ea5a6f26cb30b16a1355d989
Diogo Felipe <dlcfelipp@gmail.com>
2022-03-04 21:58:16 -0300

    feat(init): init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3000);
