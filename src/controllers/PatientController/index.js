d5d319317727355b70ae1ce127efc4e6be7e635e
Diogo Felipe <dlcfelipp@gmail.com>
2022-03-06 22:17:15 -0300

    chore(database): add new fields to API patient routes

--- src/controllers/PatientController/index.js
+++ src/controllers/PatientController/index.js
@@ -1,33 +1,19 @@
-const connection = require('../../database/connection');
+const connection = require("../../database/connection");
 
 module.exports = {
-  async index(request, response){
-    const { cpf } = request.query;
+  async index(request, response) {
+    const { doctorCpf } = request.query;
 
-    const patient = await connection('patient')
-      .select('patient.cpf', 'patient.name', 'patient.responsibleName', 'doctor.name as doctorName')
-      .join('doctor', 'patient.doctorCpf', '=', 'doctor.cpf')
-      .where('patient.cpf', cpf)
-      .first();
-
-    if(!patient){
-      return response.status(404).json({ error: 'Patient not found' });
-    }
+    const patient = await connection("patient")
+      .select(
+        "patient.cpf",
+        "patient.name",
+        "patient.responsibleName",
+        "patient.gender"
+      )
+      .join("doctor", "patient.doctorCpf", "=", "doctor.cpf")
+      .where("patient.doctorCpf", doctorCpf);
 
     return response.status(200).json(patient);
   },
-
-  async create(request, response){
-
-    const { cpf, name, responsibleName, doctorCpf } = request.body;
-    
-    const user = await connection('patient').insert({
-      cpf, 
-      name,
-      responsibleName,
-      doctorCpf,
-    })
-
-    return response.json(user);
-  }
-}
+};
