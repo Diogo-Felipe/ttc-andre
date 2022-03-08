8fe10acdaf10ef9f08b84ac3bfa7446acaab77d2
Diogo Felipe <dlcfelipp@gmail.com>
2022-03-07 23:21:31 -0300

    chore(interactions): update routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,16 @@ const PatientProfileController = require("../controllers/PatientProfileControlle
 const PatientController = require("../controllers/PatientController");
 const DoctorController = require("../controllers/DoctorController");
 const DoctorLoginController = require("../controllers/DoctorLoginController");
+const InteractionController = require("../controllers/InteractionController");
+const HasInteractionController = require("../controllers/HasInteractionController");
+const AllInteractionsController = require("../controllers/AllInteractionsController");
 
 module.exports = {
   PatientProfileController,
   PatientController,
   DoctorController,
   DoctorLoginController,
+  InteractionController,
+  HasInteractionController,
+  AllInteractionsController
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,12 @@ const {
   PatientController,
   DoctorController,
   DoctorLoginController,
+  InteractionController,
+  HasInteractionController,
+  AllInteractionsController,
 } = require("../controllers");
 
-const {
-  verifySession
-} = require("./middlewares")
+const { verifySession } = require("./middlewares");
 
 const routes = express.Router();
 
@@ -23,5 +24,16 @@ routes.delete("/doctor", DoctorController.delete);
 
 routes.get("/patient", PatientProfileController.index);
 routes.post("/patient", verifySession, PatientProfileController.create);
+routes.delete("/patient", verifySession, PatientProfileController.delete);
+
+routes.get("/interaction", InteractionController.index);
+routes.post("/interaction", verifySession, InteractionController.create);
+routes.delete("/interaction", verifySession, InteractionController.delete);
+
+routes.get("/hasInteraction", HasInteractionController.index);
+routes.post("/hasInteraction", verifySession, HasInteractionController.create);
+routes.delete("/hasInteraction", verifySession, HasInteractionController.delete);
+
+routes.get("/allInteractions", AllInteractionsController.index);
 
 module.exports = routes;
