37b2809fe8e1dbfc8c5ca81566fe63d7204a223b
Diogo Felipe <dlcfelipp@gmail.com>
2022-03-07 23:20:36 -0300

    feat(interactions): has interaction controller

---
+++ src/controllers/HasInteractionController/index.js
@@ -0,0 +1,51 @@
+const connection = require("../../database/connection");
+
+module.exports = {
+  async index(request, response) {
+    const { cpf } = request.query;
+
+    const interaction = await connection("hasInteraction")
+      .select("patient.name", "interaction.name", "interaction.description")
+      .join("patient", "patient.cpf", "=", "hasInteraction.cpf")
+      .join("interaction", "interaction.id", "=", "hasInteraction.id")
+      .where("patient.cpf", cpf);
+
+    if (!interaction) {
+      return response.status(404).json({ error: "User do not have this interaction" });
+    }
+
+    return response.status(200).json(interaction);
+  },
+
+  async create(request, response) {
+    const { cpf, id } = request.body;
+
+    const interaction = await connection("hasInteraction")
+      .select("*")
+      .where("cpf", cpf)
+      .andWhere("id", id)
+      .first();
+
+    if (interaction) {
+      return response.status(400).json({ error: "User already has this interaction" });
+    }
+
+    await connection("hasInteraction").insert({
+      cpf,
+      id,
+    });
+
+    return response.status(201).json({ cpf, id });
+  },
+
+  async delete(request, response) {
+    const { cpf, id } = request.query;
+
+    const interaction = await connection("hasInteraction")
+      .where("cpf", cpf)
+      .where("id", id)
+      .delete();
+
+    return response.status(204).json(interaction);
+  },
+};
